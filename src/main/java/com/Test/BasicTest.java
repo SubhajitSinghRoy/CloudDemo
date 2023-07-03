f1ec1990f4e93d326a10c89e93a8eb06c836f033
Subhajit Singh Roy <subhojits74@gmail.com>
2023-07-03 13:54:54 +0530

    updated to testNG tests

--- src/main/java/com/Test/BasicTest.java
+++ src/main/java/com/Test/BasicTest.java
@@ -2,11 +2,14 @@ package com.Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasicTest {
 
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public  void main() throws InterruptedException {
 		 WebDriverManager.chromedriver().setup();
 		 WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
